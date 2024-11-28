9766281ad1a26f27a1c72b6d1d2f1d1d2d5c536b
locnguyen <locnguyen071102@gmail.com>
2024-11-28 10:49:37 +0700

    refector: update codes

--- src/modules/system/question/enum/category.enum.ts
+++ src/modules/system/question/enum/category.enum.ts
@@ -1,14 +1,14 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum CategoryEnum {
-  // EASSAY = 'essay',
+  EASSAY = 'essay',
   SINGLE_CHOICE = 'single_choice',
   MULTIPLE_CHOICE = 'multiple_choice',
   TRUE_FALSE = 'true_false',
   FILL_IN = 'fill_in',
-  // MATCHING = 'matching',
-  // ORDERING = 'ordering',
-  // IMAGE_BASED = 'image_based',
+  MATCHING = 'matching',
+  ORDERING = 'ordering',
+  IMAGE_BASED = 'image_based',
 }
 
 registerEnumType(CategoryEnum, {
